63315755cf1bcc8e1880fc67033aee3f18ca6df3
Deepanshu Rajput <deepanshurajput9242.com>
2026-07-01 17:00:23 +0530

    data_types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+//alert(2+2) // we are using nodejs,not browser
+
+
+console.log(3+3);
+console.log("hitesh");//code readability 
+ 
+
+let name="Deepanshu"
+let age=20
+let ifLoggedIn=true
+// number=> 2 to power 53
+//bigint
+//string=""
+//boolean=>true/false
+//null => standalone /null ek representation h empty value ka 
+//undefined=> 
+//symbol =.> jb unique ki baat krte h tb symbol ki baat hoti h 
+
+
+//object
+console.log(typeof age);
+console.log(typeof null);
+
+
