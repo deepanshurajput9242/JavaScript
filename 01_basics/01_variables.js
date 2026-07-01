3fd23bccec98cd6dd6256c127b0cada15f68fd99
Deepanshu Rajput <deepanshurajput9242.com>
2026-07-01 17:02:44 +0530

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,4 +4,20 @@ var accountPasswords="12345"
 accountCity="Jaipur"
 // accountId=2// not allowed
 console.log(accountId);
+ accountEmail="hc@hc.com"
+ accountCity="Benguluru"
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPasswords
 
+ ]);
+ /*
+ Prefer Not to use var because of issue in block scope
+
+
+
+
+ */
+let lucky;
+console.log(lucky);
+
+ 
