00e05f75f5bd0c675ab6ac1eea435a0be1ed3167
Deepanshu Rajput <deepanshurajput9242.com>
2026-07-02 17:16:36 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score="33" //input:true/output:1//33abc/Nan//null/0//undefined//Nan //true/1//hitesh/Nan
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);//casting k liye use krskte h
+
+console.log(valueInNumber);
+
+
+
+//"33"=>33
+//"33abc"=> Nan
+//true=>1
+//false=>0
+
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1=>true;0=.false
+//""=>false
+//"hitesh"=>true
+
+let someNumer=33
+let stringNumber= String(someNumer)
+console.log(stringNumber);
+
