d8bfb6e11fb135b6b315bd7d8cb36c699f151694
Deepanshu Rajput <deepanshurajput9242.com>
2026-07-08 01:02:55 +0530

    number and Math part

---
+++ 01_basics/08_numbers.js
@@ -0,0 +1,49 @@
+const score=400
+const balance=new Number(100);
+console.log(balance);
+
+
+console.log(balance.toString());//string m convert hogya
+
+console.log(balance.toFixed(2));//isme after decimal aajayega output 100.00 mtlb precise krne k liye 
+
+const otherNumber=23.8996
+
+console.log(otherNumber.toPrecision(3));//23.8996 h toh iska output aayega 23.9
+
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++//////////////////////////////////////////////
+console.log(Math);
+console.log(Math.abs(-4));// NEGATIVE KO POSITIVE KRDETA H 
+console.log(Math.round(4.6));
+console.log(Math.ceil(6.8));
+console.log(Math.floor(6.8));
+
+console.log(Math.min(4,3,5,8));
+console.log(Math.min(4,3,5,8));
+
+console.log(Math.random());//0 se 1 m deti h 
+console.log(Math.floor(Math.random()*10)+1); //isiliye hmne 10 se multipy krke 1 add krdiya 
+
+
+const min=10
+const max=20
+
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
+
+
+
+
+
+
+
